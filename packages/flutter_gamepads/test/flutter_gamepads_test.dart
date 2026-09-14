2764934fd3c12b73f439320564299680d50af9ea
Lisette Linse <lisette.linse@gmail.com>
2026-09-14 22:46:27 +0200

    add: Test for fallback context mechanism

--- packages/flutter_gamepads/test/flutter_gamepads_test.dart
+++ packages/flutter_gamepads/test/flutter_gamepads_test.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_gamepads/src/api/gamepad_activator.dart';
 import 'package:flutter_gamepads/src/gamepad_control.dart';
 import 'package:flutter_gamepads/src/gamepad_interceptor.dart';
 import 'package:flutter_test/flutter_test.dart';
@@ -14,6 +15,8 @@ final platformInterface =
         as MethodChannelGamepadsPlatformInterface;
 
 enum _UiButton { noButton, first, second }
+
+class TestIntent extends Intent {}
 
 void main() {
   Gamepads.normalizer = GamepadNormalizer.forPlatform(
@@ -207,6 +210,104 @@ void main() {
     expect(interceptorBeforeIntentCalled, isTrue);
     expect(secondPressed, isFalse);
   });
+
+  final focusIntentsTestShortcuts = {
+    // Only use buttons with same name in GamepadButton as in gamepads_windows,
+    // or the test will break.
+    GamepadActivatorButton.dpadLeft(): TestIntent(),
+    GamepadActivatorButton.dpadUp(): PreviousFocusIntent(),
+    GamepadActivatorButton.dpadDown(): NextFocusIntent(),
+    GamepadActivatorButton.x(): DirectionalFocusIntent(TraversalDirection.up),
+    GamepadActivatorButton.y(): DirectionalFocusIntent(TraversalDirection.left),
+    GamepadActivatorButton.b(): DirectionalFocusIntent(
+      TraversalDirection.right,
+    ),
+    GamepadActivatorButton.a(): DirectionalFocusIntent(TraversalDirection.down),
+  };
+
+  for (final MapEntry(key: activator, value: intent)
+      in focusIntentsTestShortcuts.entries) {
+    // The purpose of this test is to verify that for focus change
+    // intents, GamepadControl will use fallback context to invoke
+    // the intents. For other intents, the input is dropped.
+    testWidgets('FocusIntents $activator => $intent', (
+      WidgetTester tester,
+    ) async {
+      Intent? beforeIntent;
+      var focusActionPerformed = false;
+      var otherActionPerformed = false;
+      final aFocusNode = FocusNode();
+      addTearDown(aFocusNode.dispose);
+      final focusAction = CallbackAction(
+        onInvoke: (_) => focusActionPerformed = true,
+      );
+      final otherAction = CallbackAction(
+        onInvoke: (_) => otherActionPerformed = true,
+      );
+
+      final widget = MaterialApp(
+        home: Actions(
+          actions: {
+            PreviousFocusIntent: focusAction,
+            NextFocusIntent: focusAction,
+            DirectionalFocusIntent: focusAction,
+            TestIntent: otherAction,
+          },
+          child: GamepadControl(
+            shortcuts: focusIntentsTestShortcuts,
+            onBeforeIntent: (activator, intent) {
+              beforeIntent = intent;
+              return true;
+            },
+            child: Column(
+              children: [
+                ElevatedButton(
+                  focusNode: aFocusNode,
+                  onPressed: () => {},
+                  child: const Text('First'),
+                ),
+              ],
+            ),
+          ),
+        ),
+      );
+      await tester.pumpWidget(widget);
+      FocusManager.instance.rootScope.requestScopeFocus();
+      await tester.pump();
+      // No primary focus context before performing gamepad event
+      expect(primaryFocus?.context, isNull);
+      expect(aFocusNode.hasFocus, isFalse);
+      // Primary focus is at root scope
+      expect(primaryFocus, same(FocusManager.instance.rootScope));
+
+      switch (activator) {
+        case GamepadActivatorAxis _:
+          throw Exception('Test only uses buttons');
+        case GamepadActivatorButton _:
+          // True for the buttons used in shortcuts, but not all buttons in
+          // gamepads_windows.
+          final windowsName = activator.button.name;
+          _keyPress(windowsName);
+      }
+
+      await tester.pumpAndSettle();
+      await tester.pumpAndSettle();
+
+      // Only Focus intents should cause onBeforeIntent to be called
+      final isFocusIntent =
+          intent is PreviousFocusIntent ||
+          intent is NextFocusIntent ||
+          intent is DirectionalFocusIntent;
+      expect(beforeIntent, isFocusIntent ? isNotNull : isNull);
+
+      // The focus intents should be executed all the way into casing primary
+      // focus to change
+      expect(focusActionPerformed, isFocusIntent ? isTrue : isFalse);
+      // The non-focus actions are never performed, due to primaryFocus.context
+      // is null
+      expect(otherActionPerformed, isFalse);
+    });
+  }
 }
 
 Future<void> _keyPress(String key) async {
